3239a60cc4f2c7262275699af77982a106ed1ae5
JohnConstantion <1906172933@qq.com>
2020-11-17 22:04:54 +0800

    hello world

---
+++ foodie-api/src/main/java/com/john/Application.java
@@ -0,0 +1,15 @@
+package com.john;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author constantinejohn
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
